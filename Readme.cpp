e62042d93dccb7bac1ed39562b87c827779f2ede
Tushar Barhate <tushar.barhate24@sanjivani.edu.in>
2024-08-25 12:28:06 +0530

    Create Readme.cpp

---
+++ Readme.cpp
@@ -0,0 +1,15 @@
+/*This is a CPP Program defines a simple Car class with some attributes.
+
+#include<iostream>
+#include<string>.........This is a header files
+
+class and int main() .........It is return type of main function.
+{
+}...........It is Open and Close Curly braces
+\n................This is next line function
+return 0;...............It is main function return NULL value
+
+Output :
+Land Rover Range Rover 2022
+Mustang GT 2020
+*/
